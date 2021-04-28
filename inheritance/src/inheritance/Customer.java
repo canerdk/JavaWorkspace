168031ff6de88d660ba5de532d345b2b8c32d3ed
caner <canerdk1995@hotmail.com>
2021-04-29 00:01:06 +0300

    added objects

--- inheritance/src/inheritance/Customer.java
+++ inheritance/src/inheritance/Customer.java
@@ -1,11 +1,6 @@
 package inheritance;
 
 public class Customer {
-	private int id;
-	private String customerNumber;
-	private String firstName;
-	private String lastName;
-	private String nationalIdentity;
-	private String companyName;
-	private String taxNumber;
+	int id;
+	String customerNumber;
 }
--- inheritance/src/inheritance/Main.java
+++ inheritance/src/inheritance/Main.java
@@ -3,7 +3,18 @@ package inheritance;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.customerNumber = "12345";
+		
+		
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.customerNumber= "56789";
+		
+		CustomerManager customerManager = new CustomerManager();
+
+		Customer[] customers = {individualCustomer, corporateCustomer};
+		customerManager.AddMultiple(customers);
+		
 
 	}
 
